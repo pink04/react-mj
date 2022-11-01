9f4e1408202d8f23f28c737df907d7b4ca0262ed
이민지 <alswl4@gmail.com>
2022-11-01 17:50:34 +0900

    AA

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import ParticlesBg from 'particles-bg';
-import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
